d112887a4fd7456f0fb0e02e8ddd68997667d22c
Dang Quang Hoang <hoangdq252@gmail.com>
2019-05-29 21:18:28 +0700

    update 290519

--- demo/src/sortalgo/Swap.java
+++ demo/src/sortalgo/Swap.java
@@ -10,24 +10,8 @@ import javafx.animation.*;
 import javafx.util.Duration;
 
 public class Swap{
-    // swap two adjacent numbers
+    
     ParallelTransition swap(StackPane sp1, StackPane sp2, ArrayList<StackPane> list, double speed){
-        TranslateTransition tt1 = new TranslateTransition();
-        TranslateTransition tt2 = new TranslateTransition();
-        tt1.setDuration(Duration.millis(speed));
-        tt2.setDuration(Duration.millis(speed));
-        ParallelTransition pt = new ParallelTransition();
-        tt1.setNode(sp1);
-        tt2.setNode(sp2);
-        tt1.setByX(60);
-        tt2.setByX(-60);
-        pt.getChildren().addAll(tt1, tt2);
-        Collections.swap(list, list.indexOf(sp1), list.indexOf(sp2));
-        return pt;
-    }
-
-    // swap two numbers that are not adjacent
-    ParallelTransition farswap(StackPane sp1, StackPane sp2, ArrayList<StackPane> list, double speed){
         int num;
         TranslateTransition tt1 = new TranslateTransition();
         TranslateTransition tt2 = new TranslateTransition();
